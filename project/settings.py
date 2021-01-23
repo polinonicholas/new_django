6d263034826f89137003935d8a07e9b1f0042676
Nicholas Polino <polinonicholas@gmail.com>
2021-01-22 22:06:02 -0600

    secret, redis

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=6=7jdn#+y+hdmkeg@!^7cmmdx!$t33v$27yv6ouzf6$&(toko'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
